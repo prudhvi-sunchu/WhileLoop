56e222fad060164bbaf374ee3bc35bb88183c5ba
prudhvi <prudhvi@MSI>
2023-08-10 17:33:06 +0530

    Add project files.

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        //For Loop
+        for (int i = 1; i < 10; i++)
+        {
+            if (i == 5)
+            {
+                //break; //values form 1 to 5 printed
+                continue;// values except 5 all other get printed in the loop
+            }
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+    }
+}
